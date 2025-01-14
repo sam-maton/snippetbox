a403fdbb7f0a953c9ca57bb0107eb374fa71c361
Sam Maton <sam.maton91@gmail.com>
2025-01-14 14:19:57 +0000

    setup basic templating

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,7 +11,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("starting server on :4000")
+	log.Print("starting server on http://localhost:4000")
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
