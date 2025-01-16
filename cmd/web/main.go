222bc16a8d239ed9de52501fdcc900c651e3aa9f
Sam Maton <sam.maton91@gmail.com>
2025-01-16 14:44:16 +0000

    setup snippets model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,30 +1,64 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/sam-maton/snippetbox/internal/models"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger  *slog.Logger
+	snippet *models.SnippetModel
 }
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://Sam.Maton:@localhost:5432/snippetbox?sslmode=disable", "Postgres connection string")
 	flag.Parse()
 
-	app := application{
-		logger: slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-			AddSource: true,
-		})),
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: false,
+	}))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 
-	app.logger.Info("starting server on http://localhost" + *addr)
+	defer db.Close()
 
-	err := http.ListenAndServe(*addr, app.routes())
-	app.logger.Error(err.Error())
+	app := application{
+		logger: logger,
+		snippet: &models.SnippetModel{
+			DB: db,
+		},
+	}
+
+	logger.Info("starting server on http://localhost" + *addr)
+
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
