e9c79806058992a5a9b8c2fe6f0992adefd27ba4
Sam Maton <sam.maton91@gmail.com>
2025-01-23 08:47:06 +0000

    started form validation logic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/sam-maton/snippetbox/internal/models"
 )
@@ -66,9 +68,32 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
+
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	formErrors := make(map[string]string)
+
+	//Form Validation
+	if strings.TrimSpace(title) == "" {
+		formErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		formErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		formErrors["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		formErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(formErrors) > 0 {
+		fmt.Fprint(w, formErrors)
 		return
 	}
 
