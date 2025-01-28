ffc08a1b81e603ccabd1c4bcd33a57fd77fe5a96
Sam Maton <sam.maton91@gmail.com>
2025-01-28 21:44:15 +0000

    setup test for secure headers middleware

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/sam-maton/snippetbox/internal/assert"
+)
+
+func TestSecureHeaders(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	secureHeaders(next).ServeHTTP(rr, r)
+
+	rs := rr.Result()
+
+	assert.Equal(t, rs.Header.Get("Content-Security-Policy"), "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+	assert.Equal(t, rs.Header.Get("Referrer-Policy"), "origin-when-cross-origin")
+	assert.Equal(t, rs.Header.Get("X-Content-Type-Options"), "nosniff")
+	assert.Equal(t, rs.Header.Get("X-Frame-Options"), "deny")
+	assert.Equal(t, rs.Header.Get("X-XSS-Protection"), "0")
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
