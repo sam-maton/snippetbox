b7888cd316edcb422614c7b0e629b8c9a72a089c
Sam Maton <sam.maton91@gmail.com>
2025-01-29 14:14:01 +0000

    mocked database models

---
+++ internal/mocks/snippets.go
@@ -0,0 +1,30 @@
+package mocks
+
+import (
+	"time"
+
+	"github.com/sam-maton/snippetbox/internal/models"
+)
+
+var mockSnippet = models.Snippet{
+	ID:      1,
+	Title:   "Test snippet 1",
+	Content: "Test snippet content",
+	Created: time.Now(),
+	Expires: time.Now(),
+}
+
+type SnippetModel struct{}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 2, nil
+}
+
+func (m *SnippetModel) Get(id int) (models.Snippet, error) {
+	switch id {
+	case 1:
+		return mockSnippet, nil
+	default:
+		return models.Snippet{}, models.ErrNoRecord
+	}
+}
