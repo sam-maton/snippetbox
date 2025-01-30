a2ed00e372a1ae7000666fa7deea9196a6b916d0
Sam Maton <sam.maton91@gmail.com>
2025-01-30 21:15:36 +0000

    updated test mock UserModel struct

--- internal/mocks/users.go
+++ internal/mocks/users.go
@@ -1,6 +1,10 @@
 package mocks
 
-import "github.com/sam-maton/snippetbox/internal/models"
+import (
+	"time"
+
+	"github.com/sam-maton/snippetbox/internal/models"
+)
 
 type UserModel struct{}
 
@@ -27,5 +31,19 @@ func (m *UserModel) Exists(id int) (bool, error) {
 		return true, nil
 	default:
 		return false, nil
+	}
+}
+
+func (m *UserModel) Details(id int) (models.User, error) {
+	switch id {
+	case 1:
+		return models.User{
+			ID:      1,
+			Name:    "Tester",
+			Email:   "test@email.com",
+			Created: time.Now(),
+		}, nil
+	default:
+		return models.User{}, nil
 	}
 }
